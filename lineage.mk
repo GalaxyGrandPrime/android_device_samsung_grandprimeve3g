e9c5814457ae72cbc3814681c9a53055dc74a580
Hassan Sardar <hassansardar08@live.com>
2017-06-08 13:34:43 +0400

    Add Build Fingerprint Properties.

--- lineage.mk
+++ lineage.mk
@@ -19,3 +19,8 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G531H
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=grandprimeve3g \
+    BUILD_FINGERPRINT=samsung/grandprimeve3g/grandprimeve3g:7.1.2/N2G47W/3938523:userdebug/release-keys \
+    PRIVATE_BUILD_DESC="grandprimeve3g-userdebug 7.1.2 N2G47W 3938523 release-keys"
