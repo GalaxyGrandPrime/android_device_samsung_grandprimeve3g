146678f1b53d7e0eb10f91474f69f68a08e37dd5
Hassan Sardar <hassansardar08@live.com>
2017-07-01 11:39:39 +0000

    XOSPify
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- xosp.mk
+++ xosp.mk
@@ -1,14 +1,12 @@
-## Specify phone tech before including full_phone	
-$(call inherit-product, vendor/cm/config/telephony.mk)
-
 # Release name
-PRODUCT_RELEASE_NAME := grandprimeve3g
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/grandprimeve3g.mk)
+
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Stock build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -17,8 +15,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grandprimeve3g
-PRODUCT_NAME := lineage_grandprimeve3g
+PRODUCT_NAME := xosp_grandprimeve3g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G531H
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Root Method & Browser
+ROOT_METHOD := magisk
+USE_CHROMIUM := true
